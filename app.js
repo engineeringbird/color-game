775d63d638711dfe93962939fdd23dce6b2bfe76
Birdie <birdiedev@gmail.com>
2018-08-03 23:29:25 -0700

    some logic for game

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ var colors = [
   "rgb(255, 0, 255)"
 ];
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+  // add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+
+  // add click listeners to squares
+  squares[i].addEventListener("click", function() {
+    //grab color
+    var clickedColor = this.style.backgroundColor;
+    //compare
+    if(clickedColor === pickedColor) {
+      alert("Correct!");
+    } else {
+      alert("WRONG!!");
+    }
+    });
 }
